7bdc8cb051e883cf3928b04b70a0f24a207c0303
anitha2962 <anithasivadas92@gmail.com>
2024-03-25 14:49:30 +0530

    'first_commit'

---
+++ studentdatabase.sql
@@ -0,0 +1,10 @@
+create database StudentDatabase;
+
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Age INT
+);
+
+select * from Students
